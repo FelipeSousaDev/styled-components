2972e429815f0b5aff71da53f3be14ffdc0faa5a
Felipe Sousa <felipesousa.b1@gmail.com>
2024-10-12 12:25:22 -0300

    Adicionando os primeiros componentes

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+import { Button } from "../../components/Button/index";
+import { Header } from "../../components/Header/index"
+
+
+const Home = () => {
+    return (<>
+        <Header />  
+    </>)
+}
+
+export {Home}
